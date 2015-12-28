7fd1694d640171a3f79e2de299aa8f8a40664cb3
trevstanhope <tpstanhope@gmail.com>
2015-12-28 09:11:36 -0500

    good start

--- node/__init__.py
+++ node/__init__.py
@@ -9,11 +9,15 @@ import serial
 class DummyController:
     def __init__(self, uid="dummy"):
         self.uid = uid
-    def parse(self):
-        s = "{\"uid\":\"%s\",\"chksum\":0}" % (self.uid)
+        self.sum = 0
+        self.data = "{\"N\":0,\"P\":0,\"K\":0,\"Ca\":0}"
+    def parse(self, interval=0.1):
+        time.sleep(interval)
+        s = "{\"uid\":\"%s\",\"chksum\":%d,\"data\":%s}" % (self.uid, self.sum, self.data)
         d = json.loads(s)
-        return d
-    def checksum(self):
+        if self.checksum(d):
+            return d
+    def checksum(self, d):
         return True
         
 class Controller:
@@ -23,36 +27,56 @@ class Controller:
         if self.port.inWaiting() > 0:
             s = self.port.read()
         d = json.loads(s)
-        return s
+        return d
+    def checksum(self, d, mod=256):
+        chksum = 0
+        s = str(d)
+        s_clean = s.replace(' ', '')
+        for i in s_clean:
+            chksum += ord(i)
+        return chksum % mod
             
 class Node:
     def __init__(self, config=None):
         self.config = config
         self.threads_active = True
+        self.errors = []
         if self.config['CTRL_ON']:
-            self.controller = serial.Serial(device, baud)
+            self.controller = Controller(self.config['CTRL_DEVICE'], self.config['CTRL_BAUD'])
         else:
             self.controller = DummyController()
+        self.queue = []
         threading.Thread(target=self.watchdog, args=(), kwargs={}).start()
-            
     def watchdog(self):
         while self.threads_active == True:
-            s = self.controller.parse()
-    def ping(self, data):
+            try:
+                d = self.controller.parse()
+                self.queue.append(d)
+            except Exception as e:
+                self.threads_active == False
+                self.errors.append(e)
+    def ping(self, d):
         try:
             addr = self.config['SERVER_ADDR']
-            print addr
-            r = requests.post(addr, data={'@number': 12524, '@type': 'issue', '@action': 'show'})
+            r = requests.post(addr, data=d)
             return None
         except Exception as e:
-            print str(e)
+            print 'An Exception occured in ping(): %s' % str(e)
             return (r.status_code, r.reason)
     def run(self):
-        errors = None
         try:
-            while errors is None:
-                data = {}
-                errors = self.ping(data)
+            while (len(self.errors) == 0) and self.threads_active:
+                time.sleep(0.01)
+                n = len(self.queue)
+                print 'Queue Length: %d' % n
+                if n != 0:
+                    d = self.queue.pop()
+                    print d
+                    e = self.ping(d)
+                    if e is not None:
+                        self.errors.append(e)
+            else:
+                print self.errors
         except KeyboardInterrupt:
             print "\nexiting..."
             self.threads_active = False
